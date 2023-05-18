972b15d11667be57585522c758da7da0be7e2669
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-18 20:46:44 +0600

    connect with mongodb atlas and created .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -11,6 +12,30 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tzxjncj.mongodb.net/?retryWrites=true&w=majority`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  console.log(`Example app listening on port ${port}`);
+});
