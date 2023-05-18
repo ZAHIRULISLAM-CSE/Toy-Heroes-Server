c989761c8e48e7aa82321a421255e2bd3c4510cf
ZAHIRULISLAM-CSE <neloy1613@gmail.com>
2023-05-18 20:55:25 +0600

    receive post data from client side

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // Send a ping to confirm a successful connection
+
+    app.post("/addtoys",(req,res)=>{
+        console.log(req.body);
+    })
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
